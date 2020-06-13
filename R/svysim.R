ddd7db807a764d3dad1fc2b66727bc6fda422fc8
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-06-13 11:40:19 -0400

    add new sampling, change samp_with input to df

--- R/svysim.R
+++ R/svysim.R
@@ -1,8 +1,8 @@
 #' Sample with custom function
 #'
 #'
-#' @param population An object defined by \link{DeclareDesign::declare_design}. See
-#' help page here.
+#' @param population A dataframe, which will be wrapped internally around \link{DeclareDesign::declare_design}.
+#' See help page here.
 #' @param sampling_f A sampling function that can be passed as a handler to
 #' `declare_sampling`.
 #' @param n the sample size to draw
@@ -13,23 +13,23 @@
 #' @examples
 #'
 #' # Population Value
-#' popn <- declare_population(pop_cces)
 #' stat_dem(pop_cces)
 #'
 #' # SRS
-#' samp0  <- samp_with(popn, sample_srs, n = 1000))
+#' samp0  <- samp_with(pop_cces, sample_srs, n = 1000))
 #' stat_dem(samp0)
 #'
 #' # Oversample Higher-ed
-#' samp1  <- samp_with(popn, sample_highed, n = 1000)
+#' samp1  <- samp_with(pop_cces, sample_highed, n = 1000)
 #' stat_dem(samp1)
 #'
 #' @export
 samp_with <- function(population, sampling_f, n) {
+  population <- declare_population(population)
   draw_data(population + declare_sampling(handler = sampling_f, n = n))
 }
 
-#' Propensity Score Simulation
+#' High News Interest Oversample
 #'
 #'
 #' Propensity Score that oversamples high education and high-news interest people
@@ -51,21 +51,62 @@ samp_with <- function(population, sampling_f, n) {
 #'
 #' @source Inspired by Lauren Kennedy's code in [rstanarm](https://mc-stan.org/rstanarm/articles/mrp.html)
 #'
+#' @examples
+#' p_highed(sample_n(pop_cces, 100))
+#'
 #' @export
+#'
 p_highed <- function(data) {
   race_int <- zap_labels(data$race)
   educ_int <- zap_labels(data$educ)
   news_int <- zap_labels(data$newsint)
 
-  inv_logit_scaled(-4 + c(1/(1:5)*c(1, 0.8, 0.7, 0.6, 0.5))[race_int] +
-                     (1/(1:4)*c(0.5, 1.2, 4, 5))[educ_int] +
-                     (1/(1:4)*c(6, 1, 0.4, 0.3))[news_int])
+  inv_logit_scaled(-4 + c(1/(1:5)*c("White" = 1, "Black" = 0.8, "Hispanic" = 0.7, "Asian" = 0.6, "Other" = 0.5))[race_int] +
+                     (1/(1:4)*c("No HS" = 0.5, "Some College" = 1.2, "College" = 3.0, "Post-grad" = 4.0))[educ_int] +
+                     (1/(1:4)*c("Most" = 4.0, "Often" = 1.0, "Now and Then" = 0.4, "Hardley" = 0.3))[news_int])
 }
 
-#' @rdname p_highed
+#' @rdname p_eddem
 #'
 #' @export
 sample_highed <- function(data, n) {
+  pscore <- p_highed(data)
+  sampled_ind <- sample.int(n = nrow(data), size = n, replace = FALSE, prob = pscore)
+  data[sampled_ind, ]
+}
+
+#' High News Interest + Democrat Oversample
+#'
+#'
+#' Propensity Score that oversamples high education, high-news
+#' interest, and Democratic people
+#'
+#'
+#' @details We model the true propensity score as being a separable function of
+#'  race, education, news interest, and party.
+#'
+#' @inherit p_highed
+#'
+#' @examples
+#' p_eddem(sample_n(pop_cces, 100))
+#'
+#' @export
+p_eddem <- function(data) {
+  race_int <- zap_labels(data$race)
+  educ_int <- zap_labels(data$educ)
+  news_int <- zap_labels(data$newsint)
+  pid3_int <- zap_labels(data$pid3_leaner)
+
+  inv_logit_scaled(-4 + c(1/(1:5)*c(1, 0.8, 0.7, 0.6, 0.5))[race_int] +
+                     (1/(1:4)*c(0.5, 1.2, 4, 5))[educ_int] +
+                     (1/(1:4)*c(4.0, 1, 0.4, 0.3))[news_int] +
+                     (1/(1:8)*c(3.0, 1, 1, rep(NA, 4), 1))[pid3_int])
+}
+
+#' @rdname p_eddem
+#'
+#' @export
+sample_eddem <- function(data, n) {
   pscore <- p_highed(data)
   sampled_ind <- sample.int(n = nrow(data), size = n, replace = FALSE, prob = pscore)
   data[sampled_ind, ]
